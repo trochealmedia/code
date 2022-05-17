66958e4e9439aa58c0367336a53ab8d9989ce3e5
Jonatan Troche <90587808+trochealmedia@users.noreply.github.com>
2022-05-16 22:49:09 -0400

    Delete week02 directory

--- week02/script/script.js
+++
@@ -1,6 +0,0 @@
-let d = new Date();
-let fulldate = d.getFullYear();
-document.getElementById("currentdate").textContent = fulldate;
-
-const Update = document.lastModified
-document.querySelector("#update").textContent = Update;
